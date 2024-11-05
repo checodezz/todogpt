bcc825fd217623a8bb36df6a1f9e8e46ee44e375
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-11-05 11:28:21 +0000

    add crud apis

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const connectDB = require("./db/db.connect");
 const express = require("express");
+const taskRoutes = require("./routes/taskRoutes")
 const app = express()
 app.use(express.json())
 connectDB()
 
-// app.use("/")
 
 app.get('/', (req, res) => {
     res.send("Hello, welcome to todogpt")
 }) 
+
+app.use("/api", taskRoutes);
 
 const PORT = process.env.PORT || 3000;
 
--- server/model/task.model.js
+++ server/model/task.model.js
@@ -12,7 +12,7 @@ const taskSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['pending', 'in progress', 'completed'],
+        enum: ['pending', 'ongoing', 'completed'],
         default: 'pending'
     },
 }, {timestamps : true});
